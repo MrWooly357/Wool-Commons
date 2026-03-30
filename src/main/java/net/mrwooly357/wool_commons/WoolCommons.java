d9a356cb4a7410c5292f7f77c63c0b418dc6fe03
MrWooly357 <mrwooly357@gmail.com>
2026-03-30 21:48:54 +0300

    Small fixes.

--- src/main/java/net/mrwooly357/wool_commons/WoolCommons.java
+++ src/main/java/net/mrwooly357/wool_commons/WoolCommons.java
@@ -5,20 +5,23 @@ import net.mrwooly357.wool_commons.util.Logger;
 
 public final class WoolCommons {
 
+    public static final String NAMESPACE = "wool_commons";
     public static final Logger LOGGER;
+    private static final FilePath LATEST_LOG = FilePath.RUN.resolve("latest.log");
 
     WoolCommons() {}
 
-
     static {
         FilePath.RUN.createDirectories();
-        FilePath log = FilePath.RUN.resolve("wool_commons.log");
-        log.deleteIfExists();
-        LOGGER = new Logger("wool_commons", log);
+        LATEST_LOG.deleteIfExists();
+        LOGGER = createLatestLogLogger(NAMESPACE);
     }
-
 
     public static WoolCommons getInstance() {
         return Main.INSTANCE;
+    }
+
+    public static Logger createLatestLogLogger(String id) {
+        return new Logger(id, LATEST_LOG);
     }
 }
